5528f097d7f74ef422eacaf5fc9e74ed42e36e09
Максим <55499447+lansh1rr3@users.noreply.github.com>
2025-06-17 11:23:15 +0300

    52

--- user/models.py
+++ user/models.py
@@ -1,11 +1,9 @@
 from django.contrib.auth.models import AbstractUser
-
-from exam2.shop import models
-
+from django.db import models
 
 class User(AbstractUser):
     phone = models.CharField(max_length=15)
     birth_date = models.DateField(null=True)
 
     class Meta:
-        db_table = "custom_users"
+        db_table = 'custom_users'
